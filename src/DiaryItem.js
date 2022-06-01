8c3ae55802bf26f8e7e34c82df0b0fef457efacf
makerseok <maker7788@gmail.com>
2022-06-01 21:42:40 +0900

    데이터 삭제 함수 구현

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-const DiaryItem = ({ author, content, created_date, emotion }) => {
+const DiaryItem = ({
+  onDelete,
+  author,
+  content,
+  created_date,
+  emotion,
+  id,
+}) => {
   return (
     <div className="diary-item">
       <div className="info">
@@ -11,6 +18,16 @@ const DiaryItem = ({ author, content, created_date, emotion }) => {
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
       <div className="content">일기: {content}</div>
+      <button
+        onClick={() => {
+          console.log(id);
+          if (window.confirm(`${id + 1}번째 일기를 정말 삭제하시겠습니까?`)) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
