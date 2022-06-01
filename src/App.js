482f3cf75194520e5a66f348f944d5553be2806f
makerseok <maker7788@gmail.com>
2022-06-01 21:30:26 +0900

    상단에 data state를 정의한 뒤 데이터 추가 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-
-const dummyList = [
-  {
-    id: 1,
-    author: "김철수",
-    content: "하이 1",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "박영희",
-    content: "하이 2",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "정깔깔",
-    content: "하이 3",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "유하하",
-    content: "하이 4",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-];
-
+import { useRef, useState } from "react";
 function App() {
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
