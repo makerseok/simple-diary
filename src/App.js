af1f4430d404c90c92fabf1a5f87f001cd527e3b
makerseok <maker7788@gmail.com>
2022-06-01 21:08:06 +0900

    데이터 리스트 정의 및 map 함수를 사용해 렌더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "김철수",
+    content: "하이 1",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "박영희",
+    content: "하이 2",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "정깔깔",
+    content: "하이 3",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "유하하",
+    content: "하이 4",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
