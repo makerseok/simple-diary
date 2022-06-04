e2840e4821f6ad56ada3e02cd7978c21a0af4e2c
makerseok <maker7788@gmail.com>
2022-06-04 15:02:01 +0900

    useCallback, 함수적 갱신 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
@@ -28,7 +28,7 @@ function App() {
     getData();
   }, []);
 
-  const onCreate = ({ author, content, emotion }) => {
+  const onCreate = useCallback(({ author, content, emotion }) => {
     const created_date = new Date().getTime();
     const newItem = {
       author,
@@ -38,8 +38,8 @@ function App() {
       id: dataId.current,
     };
     dataId.current += 1;
-    setData([newItem, ...data]);
-  };
+    setData((data) => [newItem, ...data]);
+  }, []);
 
   const onDelete = (targetId) => {
     const newDiaryList = data.filter((it) => it.id !== targetId);
